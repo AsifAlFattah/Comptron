eca4a428cafaa82ed4025d1629f8710eb2b874e7
Asif Al Fattah <asifalfattah34@gmail.com>
2025-01-29 12:46:19 +0600

    Project Initilization

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+const App = () => {
+  return (
+    <div className="bg-gray-100 h-screen flex items-center justify-center">
+      <h1 className="text-3xl font-bold text-red-800">
+        Hello, Tailwind CSS!
+      </h1>
+    </div>
+  );
+}
+
+export default App;
